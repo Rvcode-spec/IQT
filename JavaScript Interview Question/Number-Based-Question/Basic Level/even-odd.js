04060ff2ea3c23fc41b0a0787c7d09871ec2a32f
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-03-27 23:54:23 +0530

    JS Level-2

---
+++ JavaScript Interview Question/Number-Based-Question/Basic Level/even-odd.js
@@ -0,0 +1,26 @@
+// let num =10;
+// if(num%2==0){
+//     console.log("Even Number");   
+// }else{
+//     console.log("Odd Number");
+// }
+
+function checknumber(num){
+    if(num%2==0){
+        console.log("Even Number"); 
+    }else{
+        console.log("Odd Number");
+    }
+}
+// checknumber(21);
+
+for(let j=0; j<=10; j++){
+    if(j%2==0){
+        console.log("Ever Number",j);
+    }else{
+        console.log("Odd Number",j);
+    }
+    // break;
+}
+
+
