60dc93687550e91f8d2e3583ad2bdc71f7151e9c
civilstudy2024@gmail.com <civilstudy2024@gmail.com>
2025-04-02 23:30:33 +0530

    Q1 Solve

--- JavaScript Interview Question/Number-Based-Question/Basic Level/even-odd.js
+++ JavaScript Interview Question/Number-Based-Question/Basic Level/even-odd.js
@@ -1,26 +1,9 @@
-// let num =10;
-// if(num%2==0){
-//     console.log("Even Number");   
-// }else{
-//     console.log("Odd Number");
-// }
-
-function checknumber(num){
-    if(num%2==0){
-        console.log("Even Number"); 
+function num(n){
+    if(n%2==0){
+        console.log("Even Number");
     }else{
         console.log("Odd Number");
+        
     }
 }
-// checknumber(21);
-
-for(let j=0; j<=10; j++){
-    if(j%2==0){
-        console.log("Ever Number",j);
-    }else{
-        console.log("Odd Number",j);
-    }
-    // break;
-}
-
-
+num(60);
